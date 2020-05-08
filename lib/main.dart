05e34636de0723fae336b6da4590e8a22edff27a
gccpacman <gccpacman@gmail.com>
2020-05-09 01:46:53 +0800

    compare numbers

--- lib/main.dart
+++ lib/main.dart
@@ -24,35 +24,69 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 5;
+  int rightDiceNumber = 1;
+  String result = 'Even';
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: FlatButton(
-                child: Image.asset('images/dice$leftDiceNumber.png'),
-                onPressed: (){
-                  setState(() {
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                    print('Left Button got pressed. leftDiceNumber = $leftDiceNumber');
-                  });
-                },
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: FlatButton(
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                    onPressed: (){
+                      setState(() {
+                        leftDiceNumber = Random().nextInt(6) + 1;
+                        if (leftDiceNumber.compareTo(rightDiceNumber) == 1) {
+                          result='Left win.';
+                        } else if (leftDiceNumber.compareTo(rightDiceNumber) == 0) {
+                          result = 'Even';
+                        } else {
+                          result='Right win.';
+                        }
+                        print('Left Button got pressed. leftDiceNumber = $leftDiceNumber');
+                      });
+                    },
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: FlatButton(
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                    onPressed: (){
+                      setState(() {
+                        rightDiceNumber = Random().nextInt(6) + 1;
+                        if (leftDiceNumber.compareTo(rightDiceNumber) == 1) {
+                          result='Left win.';
+                        } else if (leftDiceNumber.compareTo(rightDiceNumber) == 0) {
+                          result = 'Even';
+                        } else {
+                          result='Right win.';
+                        }
+                        print('Right Button got pressed. rightDiceNumber = $rightDiceNumber');
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              flex: 1,
-              child: FlatButton(
-                child: Image.asset('images/dice3.png'),
-                onPressed: (){
-                  print('Right Button got pressed.');
-                },
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Text(
+                '$result',
+                style: TextStyle(color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
+            )
           ],
         ),
       ),
