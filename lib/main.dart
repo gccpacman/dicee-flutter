bc247dbaa262ef05def301376f484ba493db62db
gccpacman <gccpacman@gmail.com>
2020-05-09 01:27:45 +0800

    statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +17,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -26,9 +35,12 @@ class DicePage extends StatelessWidget {
             Expanded(
               flex: 1,
               child: FlatButton(
-                child: Image.asset('images/dice1.png'),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
                 onPressed: (){
-                  print('Left Button got pressed.');
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    print('Left Button got pressed. leftDiceNumber = $leftDiceNumber');
+                  });
                 },
               ),
             ),
